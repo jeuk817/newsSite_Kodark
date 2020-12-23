7f941481fc4b5b893de17cf99971c033d50958eb
Ryujeuk <jeuk817@gmail.com>
2020-12-23 17:02:08 +0900

    feat: HomeHeader init

---
+++ frontend/src/components/HomeHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="homeHeader">
+    home header
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.homeHeader {
+  border: 1px solid red;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeHeader />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeHeader from '../components/HomeHeader'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeHeader
   }
 }
 </script>
