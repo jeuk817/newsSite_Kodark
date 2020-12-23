a4081f88c0a8f87a26f2d4dc5660728519b8c36f
DESKTOP-J8IUUB7\user <chyunj0430@gmail.com>
2020-12-23 16:33:58 +0900

    feat: Dto 작업

--- backend/src/main/java/com/kodark/news/dto/DailyHitDto.java
+++ backend/src/main/java/com/kodark/news/dto/DailyHitDto.java
@@ -1,5 +1,38 @@
 package com.kodark.news.dto;
 
-public class DailyHitDto {
+import java.util.Date;
 
+/*
+ * 작성자 hj
+ * 작성일 12.23
+ */
+
+public class DailyHitDto {
+	private int id;
+	private Date day;
+	private int hit;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDay() {
+		return day;
+	}
+	public void setDay(Date day) {
+		this.day = day;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	
+	@Override
+	public String toString() {
+		return "DailyHitDto [id=" + id + ", day=" + day + ", hit=" + hit + "]";
+	}
+	
 }
