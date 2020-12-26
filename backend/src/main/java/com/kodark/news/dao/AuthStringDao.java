d1c040611a578f2ef204f5eec87e8b2d94059ec4
DESKTOP-J8IUUB7\user <chyunj0430@gmail.com>
2020-12-26 12:18:19 +0900

    feat: Email AuthString

--- backend/src/main/java/com/kodark/news/dao/AuthStringDao.java
+++ backend/src/main/java/com/kodark/news/dao/AuthStringDao.java
@@ -1,5 +1,20 @@
 package com.kodark.news.dao;
 
 public class AuthStringDao {
+	//인증 생성
+	@Override
+	public void createAuthKey(String userEmail, String authKey) throws Exception { // 인증키 DB에 넣기
+		Map<String, Object> map = new HashMap<String, Object>();
 
+		map.put("userEmail", userEmail);
+		map.put("authKey", authKey);
+
+		session.selectOne(namespace + ".createAuthKey", map);
+	}
+
+	//인증 여부 확인
+	@Override
+	public void userAuth(String userEmail) throws Exception { // 인증키 일치시 DB칼럼(인증여부) false->true 로 변경
+		session.update(namespace + ".userAuth", userEmail);
+	}
 }
