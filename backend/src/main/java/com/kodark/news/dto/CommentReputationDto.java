501e2d61e7defc8bbd93a756c919b66cd59a0305
asd951415 <asd951415@naver.com>
2020-12-26 20:28:11 +0900

    mapper(FAQ, AuthString) 추가, mybatis-config 추가

--- backend/src/main/java/com/kodark/news/dto/CommentReputationDto.java
+++ backend/src/main/java/com/kodark/news/dto/CommentReputationDto.java
@@ -1,5 +1,8 @@
 package com.kodark.news.dto;
 
+import org.apache.ibatis.type.Alias;
+
+@Alias("CommentReputationDto")
 public class CommentReputationDto {
 
 }
--- backend/src/main/java/com/kodark/news/dto/FaqDto.java
+++ backend/src/main/java/com/kodark/news/dto/FaqDto.java
@@ -1,11 +1,12 @@
 package com.kodark.news.dto;
 
+import org.apache.ibatis.type.Alias;
 
 /*
  * 작성자 hj
  * 작성일 12.23
  */
-
+@Alias("FaqDto")
 public class FaqDto {
 	private int id;
 	private String title;
