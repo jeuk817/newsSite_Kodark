5f0d4d29dedef4b749bb4014fb64d32b46368b90
1432cys <skylite3879@gmail.com>
2020-12-26 14:14:17 +0900

    pass

--- backend/src/main/java/com/kodark/news/dao/UserDaoImpl.java
+++ backend/src/main/java/com/kodark/news/dao/UserDaoImpl.java
@@ -9,19 +9,19 @@ import com.kodark.news.dto.UserDto;
 @Repository
 public class UserDaoImpl implements UserDao{
 
-	@Autowired 
+	@Autowired 	
 	private SqlSession sql;
 	
 	//회원가입
 	@Override
 	public void signUp(UserDto dto) throws Exception {
-		sql.insert("Mapper.signUp", dto);
+		sql.insert("UserMapper.insertUser", dto);
 		
 	}
 	//로그인
 	@Override
 	public UserDto signIn(UserDto dto) throws Exception {
-		return sql.selectOne("Mapper.signIn", dto);
+		return sql.selectOne("UserMapper.signIn", dto);
 		
 	}
 	
