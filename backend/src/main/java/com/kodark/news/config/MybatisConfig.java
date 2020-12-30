4f430732c37a08667df88c567f235f587648f51e
asd951415 <asd951415@naver.com>
2020-12-30 09:33:45 +0900

    mybatis, mysql 연동(어노테이션으로 설정한건 동적 쿼리 설정 불가능해서 xml파일로 설정)

---
+++ backend/src/main/java/com/kodark/news/config/MybatisConfig.java
@@ -0,0 +1,16 @@
+package com.kodark.news.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+    factoryBean.setDataSource(dataSource());
+    return factoryBean.getObject();
+  }
+}
