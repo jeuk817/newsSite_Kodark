73d1527922115f0c398bbe149459a4a57e43b044
Ryujeuk <jeuk817@gmail.com>
2021-01-05 02:16:48 +0900

    feat: jwt middle save
    1. jwt 토큰 생성 성공
    2. csrf 토큰 생성 성공
    3. interceptor에서 jwt 받는것까지 성공

--- backend/src/main/java/com/kodark/news/controller/AuthController.java
+++ backend/src/main/java/com/kodark/news/controller/AuthController.java
@@ -41,6 +41,7 @@ public class AuthController {
 	
 	@GetMapping(path = "/sign-in")
 	public ResponseEntity<Map<String, Object>> signIn(HttpServletResponse response) {
+		System.out.println("/sign-in");
 		String token = jwtService.createToken("jack", (2 * 1000 * 60));
 		Map<String, Object> map = new HashMap<>();
         map.put("result", token);
@@ -51,12 +52,13 @@ public class AuthController {
         cookie.setPath("/");
         
         response.addCookie(cookie);
+        response.setHeader("Links", "</users/sign-up>; rel=\"self\", </ko/signIn>; rel=\"next\"");
 		return new ResponseEntity<>(map, HttpStatus.CREATED); // 201
 	}
 	
 	@GetMapping(path = "/t")
 	public ResponseEntity<Map<String, Object>> test() {
-		
+		System.out.println("/t");
 		
 		return new ResponseEntity<>(HttpStatus.CREATED); 
 	}
@@ -64,12 +66,10 @@ public class AuthController {
 	@PostMapping
 	public ResponseEntity<String> auth(@RequestBody Map<String, Object> body) {
 		String email = (String) body.get("email");		
-		Map<String, Object> params = new HashMap<>();		
+		Map<String, Object> params = new HashMap<>();
 		params.put("_switch", "create_auth");
-		params.put("_email", email);		
+		params.put("_email", email);
 		authProcedureService.execuAuthProcedure(params);
-		System.out.println("params:"+params);
-
 		
 		if(params.get("result_set").equals("success")) {
 			Mail mail = new Mail();
@@ -83,16 +83,16 @@ public class AuthController {
 			
 			return new ResponseEntity<>(HttpStatus.CREATED); // 201
 		} else if(params.get("result_set").equals("conflict")) {
-			return new ResponseEntity<>(HttpStatus.CONFLICT); // 409			
+			return new ResponseEntity<>(HttpStatus.CONFLICT); // 409
 		} else if(params.get("result_set").equals("not content")) {
-			return new ResponseEntity<>(HttpStatus. NO_CONTENT); // 409			
+			return new ResponseEntity<>(HttpStatus. NO_CONTENT); // 409
 		} else {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); //500
 		}
 	}
+	
 	@PatchMapping(path = "/verify")
 	public ResponseEntity<String> verify(@RequestBody Map<String, Object>body){
-		System.out.println("--------------------------------------");
 		String email = (String)body.get("email");
 		Map<String, Object>params = new HashMap<>();
 		params.put("_switch", "confirm_verify");		
@@ -101,9 +101,6 @@ public class AuthController {
 		System.out.println(params.toString());		
 		authProcedureService.execuAuthProcedure(params);
 		System.out.println("check:"+params.get("result_set"));
-		
-		
-		
 		
 		if(params.get("result_set").equals("success")) {
 			System.out.println("성공");
---
+++ backend/src/main/java/com/kodark/news/interceptors/JwtInterceptor.java
@@ -0,0 +1,46 @@
+package com.kodark.news.interceptors;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kodark.news.service.JwtService;
+
+import io.jsonwebtoken.Claims;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+	@Autowired
+	JwtService jwtService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+//		return HandlerInterceptor.super.preHandle(request, response, handler);
+		System.out.println("JwtInterceptor");
+		String jwtCookie = null;
+		Cookie[] cookies = request.getCookies();
+		for(int i = 0; i < cookies.length; i++) {
+			if(cookies[i].getName().equals("jwt")) {
+				jwtCookie = cookies[i].getValue();
+				break;
+			}
+		}
+		
+		if(jwtCookie != null) {
+			Claims claims =  jwtService.getSubject(jwtCookie);
+			System.out.println(claims.getSubject());
+//			request.setAttribute("id", o);
+//			return true;
+		} else {
+			
+		}
+		return false;
+	}
+
+}
