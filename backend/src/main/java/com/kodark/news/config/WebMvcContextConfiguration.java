e344dda73f8476e3cb57dfe01d5c677b04cebec5
vnfma0218 <62713291+vnfma0218@users.noreply.github.com>
2021-01-06 01:53:11 +0900

    git property add

--- backend/src/main/java/com/kodark/news/config/WebMvcContextConfiguration.java
+++ backend/src/main/java/com/kodark/news/config/WebMvcContextConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,11 +12,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 import com.kodark.news.interceptors.JwtInterceptor;
-import com.kodark.news.interceptors.TestInterceptor;
-
 
 @Configuration
 @EnableWebMvc
