85d2652e4941e4bce4eda1010370e401670051df
Ryujeuk <jeuk817@gmail.com>
2021-01-06 14:50:00 +0900

    feat: add logger in exception handler

--- backend/src/main/java/com/kodark/news/controller/advice/ExceptionHandler.java
+++ backend/src/main/java/com/kodark/news/controller/advice/ExceptionHandler.java
@@ -2,6 +2,8 @@ package com.kodark.news.controller.advice;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,10 +12,13 @@ import com.kodark.news.controller.advice.exceptions.UnauthorizedException;
 
 @RestControllerAdvice
 public class ExceptionHandler {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@org.springframework.web.bind.annotation.ExceptionHandler(UnauthorizedException.class)
 	protected ResponseEntity<Map<String, Object>> handleUnauthorizedException(UnauthorizedException e) {
-		System.out.println(e);
+//		System.out.println(e);
+		logger.debug(e.toString());
 		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // 401
 	}
 }
