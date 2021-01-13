e9d0e132b21cfab6bd46c5ed29634063660771bb
asd951415 <asd951415@naver.com>
2021-01-13 12:24:05 +0900

    프로시저 재 커밋

--- db/procedure/admin_procedure.sql
+++ db/procedure/admin_procedure.sql
@@ -1,7 +1,7 @@
-CREATE DEFINER=`jack`@`localhost` PROCEDURE `admin_procedure`(
-   in _switch varchar(20)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_procedure`(
+   in _switch varchar(30)
     , in _id int
-    , inout _email varchar(50)    
+	, inout _email varchar(50)    
     , inout _pwd varchar(300)
     , inout _nickName varchar(20)
     , inout _name varchar(20)    
@@ -10,34 +10,148 @@ CREATE DEFINER=`jack`@`localhost` PROCEDURE `admin_procedure`(
     , inout _gender char(1)
     , inout _image varchar(200)
     , inout _auth char(8)
+    , in _commentId int
+    , in _commentReportId int
+    , in _doneFlag varchar(1)
+    , in _delFlag varchar(1)
+    , in _status varchar(20)
    , out result_set varchar(10)
 )
 BEGIN
+   declare idCount int;
+   declare authCheck char(8);
+   declare _dataInt int;
 
-declare idCount int;
-declare authCheck char(8);
-
-	if _switch = 'create_reporter' then
-		insert into  users (email, pwd, auth) values (_email, _pwd, _auth);
-		set @result = (select id from users where email = _email);
-		insert into user_detail (user_id, nick_name, name, local, birth, gender, image) values (@result, _nickName, _name, _local, _birth, _gender, _image );
+   if _switch = 'create_reporter' then
+      insert into  users (email, pwd, auth) values (_email, _pwd, _auth);
+        set @result = (select id from users where email = _email);
+      insert into user_detail (user_id, nick_name, name, local, birth, gender, image) values (@result, _nickName, _name, _local, _birth, _gender, _image );
         
     elseif _switch = 'navigation' then
-		select count(*) into idCount from users where id = _id;
-      
-		if idCount > 0 then
-			select auth into authCheck from users where id = _id;
-
-			if authCheck = 'admin' and _email is not null then
-				set result_set = '204';
+      select count(*) into idCount from users where id = _id;
+      if idCount > 0 then
+         select auth into authCheck from users where id = _id;
+            if authCheck = 'admin' and _email is not null then
+            set result_set = '204';
             else
-				set result_set = '403';
-			end if;
-
-		elseif idCount < 0 or idCount = 0 then 
-			set result_set = '401';
+            set result_set = '403';
+         end if;
+            
+       elseif idCount < 0 or idCount = 0 then 
+         set result_set = '401';
         else 
-			set result_set = '500';
+         set result_set = '500';
+      end if; 
+	/* 댓글 신고 토글
+	   작성자 : 이종현
+    */      
+   elseif _switch = 'comment_report_toggle' then
+		select count(*) into idCount from comment where id = _commentId;
+        if idCount > 0 then
+			update comment set del_flag = _delFlag 
+			where id = _commentId
+			and del_flag != _delFlag
+			;
+			set result_set = '200';
+		elseif idCount = 0 then
+			set result_set = '404';
+		else set result_set = '500';
+		end if;
+	/* 댓글 신고 확인
+	   작성자 : 이종현
+    */
+    elseif _switch = 'comment_report_check' then
+		select count(*) into idCount from comm_report where id = _commentReportId;
+        if idCount > 0 then
+			update comm_report set done_flag = 'T' 
+			where id = _commentReportId;
+			set result_set = '200';
+		elseif idCount = 0 then
+			set result_set = '404';
+		else set result_set = '500';
 		end if; 
+	/* 댓글 신고 목록
+	   작성자 : 이종현
+    */
+	elseif _switch = 'comment_report_list' then
+		select count(*) into idCount from comment where id = _commentId;
+        if idCount > 0 then
+			select 
+            cr.id
+            , cr.reason
+            ,date_format(c.created_at, '%Y-%m-%d %H:%i:%S') as 'createdAt'
+            , cr.done_flag as doneFlag
+            , u.id as userId
+            , u.email
+            , c.id as commentId
+            , c.content as content
+            , c.del_flag as delFlag
+			from comm_report cr
+			inner join users u on cr.user_id = u.id
+			inner join comment c on cr.comment_id = c.id
+            where c.id = _commentId and cr.done_flag = _doneFlag
+			group by cr.id;
+			set result_set = '200';
+		else set result_set = '404';
+        end if;
+   /* 대기중 기사 상세 
+	  작성자 : 이종현
+   */     
+   elseif _switch = 'article_wait_detail' then
+   select reporter_id into _dataInt from article where id = _id;
+		select
+		a.id
+		,c.name as category
+		,a.title
+		,a.sub_title
+		,a.content
+		,u.id as userId
+		,u.email
+		,(select ud.name from user_detail ud inner join users u on ud.user_id = u.id where u.id = _dataInt) as name
+		from article a
+		inner join users u
+		on a.reporter_id = u.id
+        
+        left join category c
+        on a.category_id = c.id
+        where a.id = _id and a.status = _status;
+
+		set result_set = '200';
+	/* 대기중 기사 상세 이미지
+	   작성자 : 이종현
+    */
+    elseif _switch = 'article_wait_detail_image' then
+		select 
+        image
+        ,source
+        ,description 
+        from image
+        where article_id = _id;
+        
+    /* 기자 목록
+	   작성자 : 이종현
+    */	        
+	elseif _switch ='admin_reporters_list' then
+	select 
+    users.id, 
+    user_detail.name, 
+    count(distinct article.id) as articleNum, 
+    sum(article.hit) as totalHit,
+    (select count(*) from subscriber where reporter_id = users.id) as subNum
+    from users 
+    
+    inner join user_detail 
+    on 
+    users.id = user_detail.user_id
+    
+    right join article
+    on
+    users.id = article.reporter_id
+    
+    where auth = 'reporter'
+    group by users.id
+    ;
+    
+   else set result_set = '500';
    end if;
 END
--- db/procedure/article_procedure.sql
+++ db/procedure/article_procedure.sql
@@ -1,28 +1,54 @@
-CREATE DEFINER=`jack`@`localhost` PROCEDURE `article_procedure`(
-	in _switch varchar(20)
-    , inout _id int
-    , out _title varchar(200)
-    , out _content varchar(5000)
-    , out _createdAt datetime
-    , out _editedAt datetime
-    , out _hit int
-	, out _image varchar(200)
-	, out _imgDec varchar(200)
-	, out _source varchar(200)
-	, out _report_id int
-	, out _email varchar(50)
-	, out _name varchar(20)
-    , out result_set  varchar(10)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `article_procedure`(
+	in _switch varchar(50)
+	,in _id int
+    ,in _commentId int
+    ,out result_set varchar(10)
 )
 BEGIN
-if _switch = 'articleDetail' then
-	select article.id, article.title, article.content, article.created_at, article.edited_at, article.hit
-		,image.image, image.source, image.description
-		, users.id, users.email, user_detail.name
-	from article
-		inner join image on article.id = image.article_id
-		inner join users on reporter_id = users.id
-		inner join user_detail on users.id = user_detail.user_id
-	where article.id = _id;
-end if;
+declare checkData int;
+
+	CASE _switch
+    /* 기사 감정 데이터
+	   작성자 : 이종현
+    */
+    when 'article_emotion' then
+		SELECT count(*) into checkData 
+		FROM article_emotion 
+		WHERE article_id =_id;
+		
+		IF checkData > 0 THEN
+		select emotion,count(*) as count
+		from article_emotion 
+		
+		inner join emotion
+		on article_emotion.emotion_id = emotion.id
+		
+		where article_id= _id
+		group by emotion;
+		set result_set = '200';
+		END IF;
+        
+    /* 기사 대댓글 데이터
+	   작성자 : 이종현
+    */
+    when 'article_comment_reply' then
+		select
+		c.id
+		,u.id as userId
+		,u.email
+		,ud.nick_name as nickName
+		,ud.local
+		, c.content
+		,date_format(c.created_at, '%Y-%m-%d %H:%i:%S') as createdAt
+		,c.del_flag as delFlag 
+		,(select count(reputation) from comm_reputation where reputation = 'recommend' and comment_id = c.id ) as recommend
+		,(select count(reputation) from comm_reputation where reputation = 'decommend'and comment_id = c.id ) as decommend
+		from comment as c
+		join users as u on c.user_id = u.id
+		join user_detail as ud on u.id = ud.user_id
+	    where c.article_id = _id AND c.parent_id = _commentId
+		;
+    
+    else set result_set = '500';
+	end case;
 END
