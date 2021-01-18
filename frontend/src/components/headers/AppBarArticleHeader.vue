5d8fc56bbcfa9ab61ef3cd48e36e22a8402f1252
Ryujeuk <jeuk817@gmail.com>
2021-01-18 10:27:48 +0900

    tmp

---
+++ frontend/src/components/headers/AppBarArticleHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app-bar
+    absolute
+    color="white"
+    elevate-on-scroll
+    height="40px"
+    id="appBarArticleHeader"
+  >
+    <div class="appBarArticleHeaderContainer">
+      <div>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-icon>search</v-icon>
+        <span>Politics</span>
+      </div>
+      <div class="appBarArticleHeaderLogo">
+        <router-link to="/en/home">
+          <img src="../../assets/img/kodark.svg">
+        </router-link>
+      </div>
+      <div>c</div>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+#appBarArticleHeader > div {
+   padding: 0;
+}
+</style>
+
+<style scoped>
+#appBarArticleHeader {
+  position: fixed;
+}
+
+.appBarArticleHeaderContainer{
+  border: 1px solid red;
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-content: center;
+}
+
+.appBarArticleHeaderLogo {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+}
+
+.appBarArticleHeaderLogo img {
+  /* width: 300px; */
+  height: 25px;
+}
+</style>
--- frontend/src/views/ArticlePage.vue
+++ frontend/src/views/ArticlePage.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
+    <AppBarArticleHeader />
     article page
+    <HelloWorld />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
+import AppBarArticleHeader from '../components/headers/AppBarArticleHeader'
+import HelloWorld from '../components/HelloWorld'
+
 export default {
-  
+  components: {
+    AppBarArticleHeader,
+    HelloWorld
+  }
 }
 </script>
 
