7827c7c40242ef02a9f9cfee7284a237b0377c2a
vnfma0218 <62713291+vnfma0218@users.noreply.github.com>
2021-01-20 09:47:28 +0900

    pr

--- frontend/src/components/bodies/admins/ReportedArticle.vue
+++ frontend/src/components/bodies/admins/ReportedArticle.vue
@@ -48,6 +48,7 @@
           depressed
           small
           color="primary"
+          @click="onSuspend"
           >
           Mail
           </v-btn>
@@ -62,6 +63,59 @@
       :length="6"
     ></v-pagination>
   </div>
+  <!-- Email Form -->
+    <div class="sendMailForm">
+      <div class="userEmail">
+        <!-- <input type="text" value="TO: userEmail" disabled="disabled"> -->
+        <v-text-field
+          value="TO: JohnDoe@gmail.com"
+          filled
+          readonly
+          dense="false"
+          height="40px"
+          class="email"
+        ></v-text-field>
+      </div>
+      <div class="suspendReason">
+        <v-select
+          :items="items"
+          filled
+          dense="false"
+          height="25px"
+          label="Suspended Reasons" 
+          class="reasonSelect"
+        ></v-select>
+      </div>
+      <div class="content">
+        <v-textarea
+        filled
+        name="input-7-4"
+        height="250px"
+        label="Write Detail Content"
+        value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+        ></v-textarea>
+      </div>
+      <div class="btns">
+        <div class="cancleBtn">
+          <v-btn
+          depressed
+          color="indigo"
+          @click="hideForm"
+          >
+            <span>Cancle</span>
+          </v-btn>
+        </div>
+    
+        <div class="submitBtn">
+          <v-btn
+          depressed
+          color="indigo"
+          >
+            <span>SubMit</span>
+          </v-btn>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -72,6 +126,16 @@ export default {
     page: 1,
     checkbox: true,
   }),
+  methods: {
+      onSuspend() {
+        const sendMailForm = document.querySelector('.sendMailForm');
+        sendMailForm.style.display = 'block';
+      },
+      hideForm() {
+        const sendMailForm = document.querySelector('.sendMailForm');
+        sendMailForm.style.display = 'none';
+      }
+  },
   async created () {
     const commentStratId = this.page;
     const { status, data } = await this.$store.dispatch('admin/getReportedArticles');
@@ -100,5 +164,46 @@ export default {
 }
 .page{
   margin-top: 50px;
+}
+
+.sendMailForm{
+  display: none;
+  margin-top: 30px;
+  border-radius: 5px;
+  border: 1px solid black;
+  height: 600px;
+  background-color: #f0f0f0;
+}
+
+.userEmail .email,
+.reasonSelect{
+  margin: 20px 0 10px 40px;
+  padding-left: 10px;
+  width: 450px;
+  height: 40px;
+  border-radius: 5px;
+  /* background-color: #f7f7f5; */
+}
+.content{
+  margin: 50px 0 10px 40px;
+  padding-left: 10px;
+  width: 800px;
+  height: 300px;
+}
+.btns{
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+}
+.submitBtn{
+  justify-self: end;
+  margin-right: 60px;
+}
+.cancleBtn{
+  justify-self: end;
+}
+
+.submitBtn span,
+.cancleBtn span{
+  color: white;
 }
 </style>
